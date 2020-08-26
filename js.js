4d0ace192c01698d6da33c280bbcda7a752c50ca
bruckervanya <54743605+bruckervanya@users.noreply.github.com>
2020-08-26 13:59:17 +0200

    php

---
+++ js.js
@@ -0,0 +1,4 @@
+function toggleOverlay() {
+        var element = document.getElementById("overlay");
+        element.classList.toggle("display--none");
+        }
---
+++ scratch.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+class Scratch {
+    public static void main(String[] args) {
+        String[] array1 = {"Prada hat", "5USD", "M", "Prada", "Hat"};
+        String[] array2 = {"Gucci Pants", "10USD", "S", "Gucci", "Pant"};
+        System.out.println(array1[0]);
+
+
+        /*Scratch myObj = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("Enter username");
+
+        String userName = myObj.nextLine();  // Read user input
+        System.out.println("Username is: " + userName);  // Output user input*/
+
+
+        // String[][] arrays = { array1, array2 };
+        System.out.println(array1[0]);
+        System.out.println(array1[1]);
+        System.out.println(array1[2]);
+        System.out.println(array1[3]);
+        System.out.println(array1[4]);
+    }
+}

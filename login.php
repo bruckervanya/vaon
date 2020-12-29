714e8baa19f4fd83c2b1474fde36b46caef2a882
bruckervanya <vanyabrucker@gmail.com>
2020-12-29 15:57:07 +0100

    php test

--- login.php
+++ login.php
@@ -1,17 +1,4 @@
 <?php
-
-$data = $_POST;
-
-$mysqli = new mysqli("rds-mysql-db.cibr9rhzokzw.us-east-1.rds.amazonaws.com","admin","xygri5-vapZok-jinhyv","bruckerpartners");
-
-// Check connection
-if ($mysqli -> connect_errno) {
-  echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
-  exit();
-}
-
-if (empty($data["username"]) || empty($data["password"])) {
-  die("Username or password missing")
-}
-
+$txt = "PHP";
+echo "I love $txt!";
 ?>

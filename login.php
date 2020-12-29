c747ecf96547ec310d91943d13620ca865a3ab39
bruckervanya <vanyabrucker@gmail.com>
2020-12-29 16:00:09 +0100

    added html to php login

--- login.php
+++ login.php
@@ -1,4 +1,14 @@
-<?php
-$txt = "PHP";
-echo "I love $txt!";
-?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+    $txt = "PHP";
+    echo "I love $txt!";
+    ?>
+  </body>
+</html>

e3a3dea6f12267b35152473294eae0225349c748
bruckervanya <vanyabrucker@gmail.com>
2020-12-29 15:51:48 +0100

    added db connection

--- login.php
+++ login.php
@@ -2,6 +2,16 @@
 
 $data = $_POST;
 
-if (empty($data["username"]) || empty($data["password"])) {
-  die("Uername or password missing")
+$mysqli = new mysqli("rds-mysql-db.cibr9rhzokzw.us-east-1.rds.amazonaws.com","admin","xygri5-vapZok-jinhyv","bruckerpartners");
+
+// Check connection
+if ($mysqli -> connect_errno) {
+  echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+  exit();
 }
+
+if (empty($data["username"]) || empty($data["password"])) {
+  die("Username or password missing")
+}
+
+?>
